c1c2e106df892ebf8072a514c8cc7117969a4eb0
scupak <anto1095@easv365.dk>
2020-08-20 11:49:44 +0200

    stuff

---
+++ CRUD-menu/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CRUD_menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] menuItems =
+            {
+                "List all videos",
+                "Add video",
+                "Delete video",
+                "Edit video",
+                "Exit"
+
+
+            }; 
+
+            var selection = 0;
+            while (selection != 5)
+            {
+                
+            
+                selection = ShowMenu(menuItems); 
+                Console.ReadLine();
+
+                switch (selection)
+                {
+
+                    case 1:
+                        Console.WriteLine("List all videos");
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("Add video");
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("Delete video");
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.WriteLine("Edit video");
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        Console.WriteLine("Exit");
+                        Console.ReadLine();
+                        break;
+
+                }
+
+
+
+
+
+
+            }
+
+        }
+
+        private static int ShowMenu(string[] menuItems)
+        {
+           Console.Clear();
+
+
+           Console.WriteLine("Select what you want to  do");
+           Console.WriteLine("");
+           /*
+           foreach (var menuItem in menuItems)
+           {
+               Console.WriteLine(menuItem);
+               
+           }
+           */
+
+           for (int i = 0; i < menuItems.Length; i++)
+           {
+               //Console.WriteLine((i +1) + ":" + menuItems[i]);
+               Console.WriteLine($"{(i + 1)}:{menuItems[i]}");
+
+            }
+
+           int selection;
+           while (!int.TryParse(Console.ReadLine(), out selection) || selection < 1 || selection > 5)
+           {
+               Console.WriteLine("You need to select a menu item");
+
+           }
+           Console.WriteLine("Selection " + selection);
+           return selection;
+        }
+    }
+}
---
+++ CRUD-menu/video.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace CRUD_menu
+{
+    class Video
+    {
+        private static int _globalId;
+        public int Id { get; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Storyline { get; set; }
+        public string Genre { get; set; }
+
+        public Video(string title, DateTime releaseDate, string storyline , string genre)
+        {
+            _globalId++;
+            Id = _globalId;
+            Title = title;
+            ReleaseDate = releaseDate;
+            Storyline = storyline;
+            Genre = genre;
+
+        }
+    }
+}
